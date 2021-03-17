0edc1f493ba1694776c0f582a11a48eed2d3cda8
mrfalcon05 <mrfalcon05@gmail.com>
2021-03-16 18:01:53 -0600

    Update admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -73,13 +73,13 @@ include('header.php');
         <div id="orders" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h2>Orders</h2>
           <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-              <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+          <div class="btn-group me-2">
+              <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
+              <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
             </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
               <span data-feather="calendar"></span>
-              This week
+              Sort By
             </button>
           </div>
         </div>

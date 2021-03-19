663227225e220c709febc52a083cf159612b65a3
Anna Sun <annasun@ITBDZBs-MacBook-Pro.local>
2021-03-18 21:58:25 -0600

    updating admin tables

--- pages/admin.php
+++ pages/admin.php
@@ -14,21 +14,15 @@ include('header.php');
         <div class="position-sticky pt-3">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link active" href="#reports">
-                <span data-feather="home"></span>
-                Reports
-              </a>
-            </li>
-            <li class="nav-item">
               <a class="nav-link" href="#orders">
                 <span data-feather="file"></span>
                 Orders
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#customers">
+              <a class="nav-link" href="#users">
                 <span data-feather="shopping-cart"></span>
-                Customers
+                Users
               </a>
             </li>
             <li class="nav-item">
@@ -60,29 +54,10 @@ include('header.php');
       </nav>
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div id="reports" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h2>Reports</h2>
-          <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-              <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-              <span data-feather="calendar"></span>
-              This week
-            </button>
-          </div>
-        </div>
-
-        <img src="../images/sales.png" style="width:90%;">
         
         <div id="orders" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h2>Orders</h2>
           <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-              <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-            </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
               <span data-feather="calendar"></span>
               Sort By
@@ -93,41 +68,46 @@ include('header.php');
           <table class="table table-striped table-sm">
             <thead>
               <tr>
+                <th>Order ID</th>
                 <th>Customer ID</th>
                 <th>Employee ID</th>
                 <th>Store ID</th>
                 <th>Purchase Date</th>
-                <th>Order ID</th>
                 <th>Invoice ID</th>
               </tr>
             </thead>
             <tbody>
-<?php
-$conn = new mysqli($hn, $un, $pw, $db);
-if($conn->connect_error) die ($conn->connect_error);
+              <?php
+              $conn = new mysqli($hn, $un, $pw, $db);
+              if($conn->connect_error) die ($conn->connect_error);
+              $query = "SELECT customer_id, employee_id, store_id, purchase_date, order_id, invoice_id FROM order_table";
+              $result = $conn->query($query);
+							if(!$result) die($conn->error);
+							$rows = $result->num_rows;
 
-$query = "SELECT customer_id, employee_id, store_id, purchase_date, order_id, invoice_id FROM saleorder";
-
-$result = $conn->query($query);
-if(!$result) die ($conn->error);
-
-while($row = $result->fetch_assoc()){
-
-echo "<tr><td>". $row["customer_id"]. "</td><td>".$row["employee_id"]. "</td><td>".$row["store_id"]. "</td><td>".
-$row["purchase_date"]. "</td><td>".$row["order_id"]."</td><td>".$row["invoice_id"]. "</td><tr>";
-}
-echo "</table>";
-?>              
+              for($j=0; $j<$rows; ++$j) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                echo <<<_END
+                <tr>
+                  <td>$row[order_id]</td>
+                  <td>$row[customer_id]</td>
+                  <td>$row[employee_id]</td>
+                  <td>$row[store_id]</td>
+                  <td>$row[purchase_date]</td>
+                  <td>$row[invoice_id]</td>
+                <tr>
+                _END;
+              }
+              ?>              
             </tbody>
           </table>
         </div>
 
-        <div id="customers" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h2>Customers</h2>
+        <div id="users" class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h2>Users</h2>
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
-              <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+              <button type="button" class="btn btn-sm btn-outline-secondary">Add User</button>
             </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
               <span data-feather="calendar"></span>
@@ -139,30 +119,39 @@ echo "</table>";
           <table class="table table-striped table-sm">
             <thead>
               <tr>
-                <th>Customer ID</th>
+                <th>User ID</th>
+                <th>User Name</th>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Address</th>
                 <th>Phone Number</th>
+                <th>Role</th>
               </tr>
             </thead>
             <tbody>
-<?php
-$conn = new mysqli($hn, $un, $pw, $db);
-if($conn->connect_error) die ($conn->connect_error);
+              <?php
+              $conn = new mysqli($hn, $un, $pw, $db);
+              if($conn->connect_error) die ($conn->connect_error);
+              $query = "SELECT user_id, username, first_name, last_name, address, phone_number, user_role FROM user_table";
+              $result = $conn->query($query);
+							if(!$result) die($conn->error);
+							$rows = $result->num_rows;
 
-$query = "SELECT customer_id, first_name, last_name, address, phone_number FROM customer";
-
-$result = $conn->query($query);
-if(!$result) die ($conn->error);
-
-while($row = $result->fetch_assoc()){
-
-echo "<tr><td>". $row["customer_id"]. "</td><td>".$row["first_name"]. "</td><td>".$row["last_name"]. "</td><td>".
-$row["address"]. "</td><td>".$row["phone_number"]. "</td><tr>";
-}
-echo "</table>";
-?>
+              for($j=0; $j<$rows; ++$j) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                echo <<<_END
+                <tr>
+                  <td>$row[user_id]</td>
+                  <td>$row[username]</td>
+                  <td>$row[first_name]</td>
+                  <td>$row[last_name]</td>
+                  <td>$row[address]</td>
+                  <td>$row[phone_number]</td>
+                  <td>$row[user_role]</td>
+                <tr>
+                _END;
+              }
+              ?>
             </tbody>
           </table>
         </div>
@@ -172,7 +161,6 @@ echo "</table>";
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
             </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
               <span data-feather="calendar"></span>
@@ -192,23 +180,27 @@ echo "</table>";
               </tr>
             </thead>
             <tbody>
-<?php
-$conn = new mysqli($hn, $un, $pw, $db);
-if($conn->connect_error) die ($conn->connect_error);
+              <?php
+              $conn = new mysqli($hn, $un, $pw, $db);
+              if($conn->connect_error) die ($conn->connect_error);
+              $query = "SELECT inventory_id, product_id, store_id, stock_date, stock_quantity FROM inventory";
+              $result = $conn->query($query);
+							if(!$result) die($conn->error);
+							$rows = $result->num_rows;
 
-$query = "SELECT inventory_id, product_id, store_id, stock_date, stock_quantity FROM inventory";
-
-$result = $conn->query($query);
-if(!$result) die ($conn->error);
-
-while($row = $result->fetch_assoc()){
-
-echo "<tr><td>". $row["inventory_id"]. "</td><td>".$row["product_id"]. "</td><td>".$row["store_id"]. "</td><td>".
-$row["stock_date"]. "</td><td>".$row["stock_quantity"]. "</td><tr>";
-}
-echo "</table>";
-?>
-              
+              for($j=0; $j<$rows; ++$j) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                echo <<<_END
+                <tr>
+                  <td>$row[inventory_id]</td>
+                  <td>$row[product_id]</td>
+                  <td>$row[store_id]</td>
+                  <td>$row[stock_date]</td>
+                  <td>$row[stock_quantity]</td>
+                <tr>
+                _END;
+              }
+              ?>
             </tbody>
           </table>
         </div>
@@ -218,7 +210,6 @@ echo "</table>";
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary" name="ok">Edit</button>
             </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
               <span data-feather="calendar"></span>
@@ -236,22 +227,25 @@ echo "</table>";
               </tr>
             </thead>
             <tbody>
-<?php
-$conn = new mysqli($hn, $un, $pw, $db);
-if($conn->connect_error) die ($conn->connect_error);
+              <?php
+              $conn = new mysqli($hn, $un, $pw, $db);
+              if($conn->connect_error) die ($conn->connect_error);
+              $query = "SELECT vendor_id, vendor_name, vendor_address FROM vendor";
+              $result = $conn->query($query);
+							if(!$result) die($conn->error);
+							$rows = $result->num_rows;
 
-$query = "SELECT vendor_id, vendor_name, vendor_address FROM vendor";
-
-$result = $conn->query($query);
-if(!$result) die ($conn->error);
-
-while($row = $result->fetch_assoc()){
-
-echo "<tr><td>". $row["vendor_id"]. "</td><td>".$row["vendor_name"]. "</td><td>".$row["vendor_address"]. "</td><tr>";
-}
-echo "</table>";
-?>
-       
+              for($j=0; $j<$rows; ++$j) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                echo <<<_END
+                <tr>
+                  <td>$row[vendor_id]</td>
+                  <td>$row[vendor_name]</td>
+                  <td>$row[vendor_address]</td>
+                <tr>
+                _END;
+              }
+              ?>   
             </tbody>
           </table>
         </div>
@@ -261,7 +255,6 @@ echo "</table>";
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary" name="ok">Edit</button>
             </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
               <span data-feather="calendar"></span>
@@ -280,23 +273,26 @@ echo "</table>";
               </tr>
             </thead>
             <tbody>
-<?php
-$conn = new mysqli($hn, $un, $pw, $db);
-if($conn->connect_error) die ($conn->connect_error);
+              <?php
+              $conn = new mysqli($hn, $un, $pw, $db);
+              if($conn->connect_error) die ($conn->connect_error);
+              $query = "SELECT store_id, store_address, store_hours, store_type FROM store";
+              $result = $conn->query($query);
+							if(!$result) die($conn->error);
+							$rows = $result->num_rows;
 
-$query = "SELECT store_id, store_address, store_hours, store_type FROM store";
-
-$result = $conn->query($query);
-if(!$result) die ($conn->error);
-
-while($row = $result->fetch_assoc()){
-
-echo "<tr><td>". $row["store_id"]. "</td><td>".$row["store_address"]. "</td><td>".$row["store_hours"]. "</td><td>".
-$row["store_type"]. "</td><tr>";
-}
-echo "</table>";
-?>
-       
+              for($j=0; $j<$rows; ++$j) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                echo <<<_END
+                <tr>
+                  <td>$row[store_id]</td>
+                  <td>$row[store_address]</td>
+                  <td>$row[store_hours]</td>
+                  <td>$row[store_type]</td>
+                <tr>
+                _END;
+              }
+              ?>
             </tbody>
           </table>
         </div>
@@ -306,7 +302,6 @@ echo "</table>";
           <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary" name="ok">Edit</button>
             </div>
             <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
               <span data-feather="calendar"></span>
@@ -326,23 +321,27 @@ echo "</table>";
               </tr>
             </thead>
             <tbody>
-<?php
-$conn = new mysqli($hn, $un, $pw, $db);
-if($conn->connect_error) die ($conn->connect_error);
+              <?php
+              $conn = new mysqli($hn, $un, $pw, $db);
+              if($conn->connect_error) die ($conn->connect_error);
+              $query = "SELECT employee_id, last_name, first_name, employee_title, store_id FROM employee";
+              $result = $conn->query($query);
+							if(!$result) die($conn->error);
+							$rows = $result->num_rows;
 
-$query = "SELECT employee_id, last_name, first_name, employee_title, store_id FROM employee";
-
-$result = $conn->query($query);
-if(!$result) die ($conn->error);
-
-while($row = $result->fetch_assoc()){
-
-echo "<tr><td>". $row["employee_id"]. "</td><td>".$row["last_name"]. "</td><td>".$row["first_name"]. "</td><td>".
-$row["employee_title"]. "</td><td>".$row["store_id"]. "</td><tr>";
-}
-echo "</table>";
-?>
-       
+              for($j=0; $j<$rows; ++$j) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                echo <<<_END
+                <tr>
+                  <td>$row[employee_id]</td>
+                  <td>$row[last_name]</td>
+                  <td>$row[first_name]</td>
+                  <td>$row[employee_title]</td>
+                  <td>$row[store_id]</td>
+                <tr>
+                _END;
+              }
+              ?>
             </tbody>
           </table>
         </div>
